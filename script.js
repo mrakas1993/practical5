fe76627ad6cc8f0badf988585cc93cb9d5850e27
Andrey <mrakas93@yandex.ru>
2021-10-13 20:41:26 +0300

    practical5

---
+++ script.js
@@ -0,0 +1,42 @@
+'use strict';
+// Задача 1
+// Запрограммируйте поведение кнопки по нажатию на нее (она меняет текст в 
+// span)
+// function buttonClick(){
+//     let elem = document.getElementById('elem');
+//     elem.innerHTML = '1';
+// }
+// Задача 2
+// Запрограммируйте  поведение кнопки по нажатию на нее (она меняет span на тег b, 
+// не изменяя при этом текст внутри тега)
+// function buttonClick(){
+//     let elem = document.getElementById('elem');
+//     elem.outerHTML = '<b>'+elem.innerHTML+'</b>';
+// }
+// Задча 3
+// Дан HTML код. Поменяйте содержимое абзацев на их порядковый номер в 
+// коде.
+// function func(){
+//     let elems = document.getElementsByTagName('p');
+//     for(let i = 0; i<elems.length; i++){
+//         elems[i].innerHTML = i+1;
+//     }
+// }
+// Задача 4
+// Дан HTML код. Поменяйте содержимое элементов с классом "а" на их 
+// порядковый номер в коде.
+// function func(){
+//     let elems = document.getElementsByClassName('a');
+//     for(let i = 0;i<elems.length;i++){
+//         elems[i].innerHTML = i+1;
+//     }
+// }
+// Задача 5
+// Дан HTML код. Поменяйте содержимое абзацев с классом "а" на их 
+// порядковый номер в коде
+// function func(){
+//         let elems = document.querySelectorAll('.a');
+//         for(let i = 0;i<elems.length;i++){
+//             elems[i].innerHTML = i+1;
+//         }
+// }
